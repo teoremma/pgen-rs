0f92b3dfb8935391c14c76d3a599616490d8a87d
emmanuel <emmanuelanaya@outlook.com>
2024-02-28 04:02:32 +0000

    Add test pgens and initial checks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod reader;
+
 fn main() {
-    println!("Hello, world!");
+    let test_pgens = vec![
+        "data/sample1/1000G_chr19_pruned.pgen",
+        "data/sample2/pset1_1000G_chr16.pgen",
+        "data/random1/random1.pgen",
+    ];
+    for pgen_path in test_pgens {
+        println!("testing file: {}", pgen_path);
+        reader::read_header(pgen_path).unwrap();
+        println!("\n");
+    }
 }
